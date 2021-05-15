273980b0c984d3232db15d3c27196c70555ef240
minsgy <alstjrchl97@naver.com>
2021-05-15 17:23:39 +0900

    FIX::redux reducer 수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import reducer from './redux/reducers';
+import reducer from './Redux/reducers';
 import { Provider } from 'react-redux';
 import configureStore from './Store/store';
 
