bacc102ca7d420ddd5e631003e064ef21e23c445
qhahd78 <umuj0708@likelion.org>
2021-05-22 23:15:52 +0900

    [FE]개발:: ProjectList API Test 완료

--- client/src/Pages/Portfolio/ApiTest.js
+++ client/src/Pages/Portfolio/ApiTest.js
@@ -13,7 +13,6 @@ const SS = styled.div`
 
 function ApiTest() {
   // 변수 state 초기값 설정.
-  const [users, setUsers] = useState(null);
   const [tests, setTests] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -22,20 +21,15 @@ function ApiTest() {
     // 컴포넌트가 화면에 나타날 때 실행할 함수 선언
     const fetchUsers = async () => {
       try {
-        // 요청 시작 : error 와 users 초기화
+        // 요청 시작 : error 와 tests 초기화
         setError(null);
-        setUsers(null);
         setTests(null);
         // loading 값 true
         setLoading(true);
         // 응답을 res 에 저장.
-        const res2 = await axios.get('http://localhost:5000/projects/lists');
-        const res = await axios.get(
-          'https://jsonplaceholder.typicode.com/users',
-        );
-        // users 에 res 안에 있는 data 를 저장.
-        setUsers(res.data);
-        setTests(res2.data);
+        const res = await axios.get('projects/lists');
+        // console.log(res.data[0]);
+        setTests(res.data[0]);
       } catch (e) {
         // 만약 오류가 생기면 여기서 catch
         setError(e); // error : true
@@ -44,6 +38,7 @@ function ApiTest() {
     };
     // 함수 fetchUsers() 실행
     fetchUsers();
+    console.log(tests);
   }, []);
   if (loading)
     return (
@@ -52,18 +47,13 @@ function ApiTest() {
       </SS>
     ); // loading == true
   if (error) return <div>에러 발생</div>; // error == true
-  if (!users) return null; // users == false
-  if (!tests) return null;
+  if (!tests) return null; // tests == false
   return (
     <div>
       <p>api 테스트 페이지</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.username} ({user.name})
-          </li>
-        ))}
-      </ul>
+      <p>{tests.title}</p>
+      <p>{tests.role}</p>
+      <p>{tests.startDate}</p>
     </div>
   );
 }
