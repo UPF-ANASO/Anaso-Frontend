ec4c8bed32b3a26c4456a4faab33b64f861cc588
qhahd78 <umuj0708@likelion.org>
2021-05-22 21:58:46 +0900

    [FE]개발::api instace setting, projectlist api test setting

---
+++ client/src/Api/Project/Project.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import React from 'react';
+import { instance } from '../Server';
+
+export const ProjectListAPI = () => {
+  //   const list =await
+  //   console.log(list.data);
+  //   return title, role;
+};
