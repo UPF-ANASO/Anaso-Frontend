d8bde1c8dbcef31b1d59640c5afd7ec7054da75e
qhahd78 <umuj0708@likelion.org>
2021-06-10 23:12:41 +0900

    [개발] :: portfolioCreate 진행 중

--- client/src/Api/Project/Project.js
+++ client/src/Api/Project/Project.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
+// 프로젝트 목록 API
 export const ProjectListAPI = async () => {
   const projectlist = await axios.get('projects/lists');
   return projectlist;
 };
 
-export const ProjectCreateAPI = async () => {
+// 프로젝트 생성 API
+export const ProjectCreateAPI = async (
+  title,
+  participant,
+  endDate,
+  thumbnail,
+  role,
+) => {
   const api = axios.post('projects/create', {
     title: '고양이는 귀여워 프로젝트',
     participant: '밈미, 옹이',
