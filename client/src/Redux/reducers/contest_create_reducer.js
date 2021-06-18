e2bac23e2e6d402795ab5bf8d2015216fcf215ac
white-jang <galaxy__12@naver.com>
2021-06-19 00:44:51 +0900

    개발 :: Position 관련 Redux 설계 (미완)

--- client/src/Redux/reducers/contest_create_reducer.js
+++ client/src/Redux/reducers/contest_create_reducer.js
@@ -3,12 +3,27 @@ import { createAction, handleActions } from 'redux-actions';
 const INITIALIZE = 'contest_create/INITIALIZE'; // 내용 초기화
 const CHANGE_FIELD = 'contest_create/CHANGE_FIELD'; // 특정 key 값 바꾸기
 // key 값은 무슨 값인지 저장 ex) 'contestName', 'title' ...
+const PUSH_POSITION = 'contest_create/PUSH_POSITION'; // 포지션 넣기
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
   key,
   value,
 }));
+export const pushPosition = createAction(
+  PUSH_POSITION,
+  ({
+    positionNameKey,
+    positionNameValue,
+    recuritNumberKey,
+    recuritNumberValue,
+  }) => ({
+    positionNameKey,
+    positionNameValue,
+    recuritNumberKey,
+    recuritNumberValue,
+  }),
+);
 
 const initialState = {
   contestName: '', // 공모전 이름
@@ -21,14 +36,12 @@ const initialState = {
   category: '', // 카테고리
   organizer: '', // 주최 기관
   totalMembers: 0, // 모집 인원 수
-  applyStatus: {
-    positions: [
-      {
-        positionName: '', // 포지션 이름
-        recruitNumbers: 0, // 해당 포지션 총 인원 수
-      },
-    ],
-  },
+  positions: [
+    {
+      positionName: '', // 포지션 이름
+      recruitNumbers: 0, // 해당 포지션 총 인원 수
+    },
+  ],
 };
 
 const contestCreate = handleActions(
@@ -37,6 +50,19 @@ const contestCreate = handleActions(
     [CHANGE_FIELD]: (state, { payload: { key, value } }) => ({
       ...state,
       [key]: value, // 특정 key 값 업데이트
+    }),
+    [PUSH_POSITION]: (
+      state,
+      { payload: { positionNameValue, recuritNumberValue } },
+    ) => ({
+      ...state,
+      positions: [
+        ...state.positions,
+        {
+          positionName: positionNameValue,
+          recruitNumbers: recuritNumberValue,
+        },
+      ],
     }),
   },
   initialState,
